7ea2f93e901c831ef88003163789ef878904e01b
pablomarquezfernandez <124263202+pablomarquezfernandez@users.noreply.github.com>
2023-11-04 11:53:05 -0500

    Metodo defecto

--- src/main/java/co/inphotech/marshando/auxiliarcontable/controllers/DefectoController.java
+++ src/main/java/co/inphotech/marshando/auxiliarcontable/controllers/DefectoController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ import com.amazonaws.util.json.JSONObject;
 public class DefectoController {
 
 	@CrossOrigin
-	@PostMapping(value = "/auxiliar-contable", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/auxiliar-contable", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> getElementoContabilizados(   ) throws Exception{
 		HttpHeaders responseHeaders = new HttpHeaders();
 	
